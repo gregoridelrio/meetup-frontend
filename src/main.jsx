1a50cca6e4b621cdec9caba9baff3ebe6fb2c938
Laurea Gregori <gregoridelrio@gmail.com>
2026-04-02 12:29:59 +0200

    feat: added global variables

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { AuthProvider } from "./context/AuthContext";
 import App from "./App";
+import "./styles/global.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
